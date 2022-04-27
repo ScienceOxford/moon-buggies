a6cb5100bba4c20108e60017294cbad344957fe0
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2022-04-27 14:00:33 +0100

    update moonbuggy.py & add single micro:bit debugging code

--- examples/debugging.py
+++ examples/debugging.py
@@ -1,5 +1,5 @@
 from microbit import *
-import radio
+#import radio
 
 LF = pin13
 LB = pin12
@@ -44,23 +44,30 @@ def mistake(error):
     sleep(5000)
     stop()
 
-radio.on()
-radio.config(channel=0, queue=10) #CHANGE CHANNEL
+#radio.on()
+#radio.config(channel=0, queue=10) #CHANGE CHANNEL
 
 on_the_moon = True
 directions = ['forward', 'left', 'right', 'backward']
 grabber = {'1': 35, '2': 40, '3': 45, '4': 50, '5': 55, '6': 60}
 
 while True:
-    message = radio.receive()
+    print()
+    #message = radio.receive()
+    message = input("type message to send: ")
+    print()
+    print("robot received: " + message)
     if message is not None:
 
         if message == 'earth':
             on_the_moon = False
+            print("robot does: " + message)
         elif message == 'moon':
             on_the_moon = True
+            print("robot does: " + message)
         elif message == 'stop':
             stop()
+            print("robot does: " + message)
 
         else:
             message = message.split()
@@ -87,18 +94,22 @@ while True:
                         drive(speed, -speed)
                     elif message[0] == 'backward':
                         drive(-speed, -speed)
-
+                    print("robot does: " + message[0] + " at speed " + str(speed) + " for " + str(time) + "ms")
                     sleep(time)
                     stop()
 
                 except:
+                    print("robot has an error: you sent a direction not the speed and time were incorrect")
                     mistake(2) # if a direction but not given both speed and time
 
             elif message[0] == 'grab':
                 if message[1] in grabber:
                     pin16.write_analog(grabber[message[1]])
+                    print("robot does: " + message[0] + " option " + message[1])
                 else:
+                    print("robot has an error: you used a grab value that was incorrect")
                     mistake(3) # if grab but not given correct option
 
             else:
+                print("robot has an error: you sent a command it did not understand, check your spelling")
                 mistake(1) # if not in directions or grab, you spelt something wrong
